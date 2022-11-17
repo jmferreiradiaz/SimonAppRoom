73ab303fd9b212a556b09e7362f89695293049cf
josem.ferreira@iesnervion.es <josem.ferreira@iesnervion.es>
2022-11-17 10:07:16 +0100

    simon terminado

--- app/src/main/java/com/example/simonapp/MainActivity.kt
+++ app/src/main/java/com/example/simonapp/MainActivity.kt
@@ -17,16 +17,31 @@ import kotlin.collections.ArrayList
 class MainActivity : AppCompatActivity(), IComunicador {
     //Lista donde guardaremos la secuencia de la máquina
     private var listaNumerosIA = arrayListOf<Int>()
+
     //Lista donde guardaremos la secuencia del jugador
     private var listaNumerosJugador = arrayListOf<Int>()
+
     //contador que guardar las pulsaciones del jugador en cada turno
-    private var cont = 0;
-    //Booleano que indica si el jugador ha sido derrotado o no
-    private var derrota = false;
+    private var cont = 0
+
     //booleano que indica si los botones son clickables para el jugador o no
-    private var isBtnClickable = false;
+    private var isBtnClickable = false
+
     //variable que indica los niveles pasados por el jugador
     private var nivel = 0
+
+    //variables de los botones
+    private lateinit var btnAmarillo: View
+    private lateinit var btnRojo:View
+    private lateinit var btnVerde:View
+    private lateinit var btnAzul:View
+    private lateinit var btnStart:Button
+
+    //variables para los sonidos
+    private lateinit var media1: MediaPlayer
+    private lateinit var media2: MediaPlayer
+    private lateinit var media3: MediaPlayer
+    private lateinit var media4: MediaPlayer
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,28 +49,37 @@ class MainActivity : AppCompatActivity(), IComunicador {
         setContentView(R.layout.activity_main)
     }
 
+    override fun onStart() {
+        super.onStart()
+        //declaramos los sonidos
+        media1 = MediaPlayer.create(this, R.raw.mi)
+        media2 = MediaPlayer.create(this, R.raw.mi1)
+        media3 = MediaPlayer.create(this, R.raw.re)
+        media4 = MediaPlayer.create(this, R.raw.sol)
+
+        //declaramos los botones
+        btnAmarillo = findViewById<View>(R.id.btnAmarillo)
+        btnRojo = findViewById<View>(R.id.btnRojo)
+        btnVerde = findViewById<View>(R.id.btnVerde)
+        btnAzul = findViewById<View>(R.id.btnAzul)
+        btnStart = findViewById<Button>(R.id.btnStart)
+    }
+
     override fun onResume() {
         super.onResume()
-        val btnStart = findViewById<Button>(R.id.btnStart)
-        //Cuando se pulse el botón Start, el juego comienza
-        btnStart.setOnClickListener{turnoIA()}
+        btnStart.setOnClickListener { turnoIA() }
     }
 
     /**
      * Método que genera la secuencia de la máquina
      */
-    fun turnoIA(){
+    fun turnoIA() {
         //Desactivamos los botones cuando la secuencia empieza
         isBtnClickable = false
-        //Ponemos derrota a false
-        derrota = false
+        btnStart.isClickable = false
+
         //Reiniciamos el contador
         cont = 0
-        //variables para los sonidos
-        val media1 = MediaPlayer.create(this,R.raw.mi)
-        val media2 = MediaPlayer.create(this,R.raw.mi1)
-        val media3 = MediaPlayer.create(this,R.raw.re)
-        val media4 = MediaPlayer.create(this,R.raw.sol)
 
         //Generamos un número aleatorio de el 0 al 3
         val numAle = Random(System.nanoTime()).nextInt(4)
@@ -65,44 +89,11 @@ class MainActivity : AppCompatActivity(), IComunicador {
 
         //Lanzamos la corutina de la iluminación de los botones
         GlobalScope.launch {
-            for (i in 0..listaNumerosIA.size - 1){
+            for (i in 0..listaNumerosIA.size - 1) {
                 //Encendemos los colores
                 delay(500)
                 encenderColores(listaNumerosIA[i])
-                //region Sonidos
-                when(listaNumerosIA[i]){
-                    0 -> { media1.setVolume(100F,100F)
-                        if(media1.isPlaying){
-                            media1.pause()
-                            media1.seekTo(0)
-                            media1.start()
-                        } else {
-                            media1.start()
-                        }
-                    }
-                    1 -> { media2.setVolume(100F,100F)
-                        if(media2.isPlaying){
-                            media2.pause()
-                            media2.seekTo(0)
-                        }
-                        media2.start()
-                    }
-                    2 -> { media3.setVolume(100F,100F)
-                        if(media3.isPlaying){
-                            media3.pause()
-                            media3.seekTo(0)
-                        }
-                        media3.start()
-                    }
-                    3 -> { media4.setVolume(100F,100F)
-                        if(media4.isPlaying){
-                            media4.pause()
-                            media4.seekTo(0)
-                        }
-                            media4.start()
-                    }
-                }
-                //endregion
+                reproducirSonidos(listaNumerosIA[i])
                 delay(500)
                 //Apagar colores
                 apagarColores(listaNumerosIA[i])
@@ -116,17 +107,15 @@ class MainActivity : AppCompatActivity(), IComunicador {
     /**
      * Método que se inicia cuando el jugador pulsa una de las 4 vistas.
      */
-    override fun onPulsarBtnJugador(btn : View){
+    override fun onPulsarBtnJugador(btn: View) {
         //Variable donde guardaremos el número correspondiente a cada botón
         var numColor = 0
 
-        //variables para los sonidos
-        val media1 = MediaPlayer.create(this,R.raw.mi)
-        val media2 = MediaPlayer.create(this,R.raw.mi1)
-        val media3 = MediaPlayer.create(this,R.raw.re)
-        val media4 = MediaPlayer.create(this,R.raw.sol)
+        //Booleano que indica si el jugador ha sido derrotado o no
+        var derrota = false;
 
         //Se asignan los números a los botones
+        //Si el botón se puede pulsar entra
         if (isBtnClickable) {
             when (btn.id) {
                 R.id.btnRojo -> numColor = 0;
@@ -139,47 +128,15 @@ class MainActivity : AppCompatActivity(), IComunicador {
             listaNumerosJugador.add(numColor)
 
             GlobalScope.launch {
-                    //Encendemos los colores
-                    encenderColores(numColor)
-                    delay(200)
-                //region Sonidos
-                when(numColor){
-                    0 -> { media1.setVolume(100F,100F)
-                        if(media1.isPlaying){
-                            media1.pause()
-                            media1.seekTo(0)
-                            media1.start()
-                        } else {
-                            media1.start()
-                        }
-                    }
-                    1 -> { media2.setVolume(100F,100F)
-                        if(media2.isPlaying){
-                            media2.pause()
-                            media2.seekTo(0)
-                        }
-                        media2.start()
-                    }
-                    2 -> { media3.setVolume(100F,100F)
-                        if(media3.isPlaying){
-                            media3.pause()
-                            media3.seekTo(0)
-                        }
-                        media3.start()
-                    }
-                    3 -> { media4.setVolume(100F,100F)
-                        if(media4.isPlaying) {
-                            media4.pause()
-                            media4.seekTo(0)
-                        }
-                            media4.start()
-                        }
-                    }
-                }
-                //endregion
-                    //Apagar colores
-                    apagarColores(numColor)
-                }
+
+                //Encendemos los colores
+                encenderColores(numColor)
+                reproducirSonidos(numColor)
+                delay(500)
+                //Apagar colores
+                apagarColores(numColor)
+            }
+
 
             //Sí la secuencia de la ia es igual a la del jugador sigue el juego
             if (numColor == listaNumerosIA[cont]) {
@@ -192,8 +149,9 @@ class MainActivity : AppCompatActivity(), IComunicador {
                 listaNumerosJugador.clear()
                 derrota = true
                 nivel = 0;
-                findViewById<TextView>(R.id.lblNivel).text = "Nivel: "+nivel
+                findViewById<TextView>(R.id.lblNivel).text = "Nivel: " + nivel
                 isBtnClickable = false
+                btnStart.isClickable = true
             }
 
             //Si el tamaño de las 2 listas son iguales y derrota está a false
@@ -202,9 +160,13 @@ class MainActivity : AppCompatActivity(), IComunicador {
                 listaNumerosJugador.clear()
                 isBtnClickable = false
                 nivel++;
-                findViewById<TextView>(R.id.lblNivel).text = "Nivel: "+nivel
+                findViewById<TextView>(R.id.lblNivel).text = "Nivel: " + nivel
                 //Le toca a las IA
-                turnoIA()
+                GlobalScope.launch {
+                    delay(600)
+                    turnoIA()
+                }
+
             }
         }
     }
@@ -213,11 +175,7 @@ class MainActivity : AppCompatActivity(), IComunicador {
     /**
      * Cambia el color del botón actual por otro de tono más claro
      */
-    fun encenderColores (num: Int){
-        val btnAmarillo = findViewById<View>(R.id.btnAmarillo)
-        val btnRojo = findViewById<View>(R.id.btnRojo)
-        val btnVerde = findViewById<View>(R.id.btnVerde)
-        val btnAzul = findViewById<View>(R.id.btnAzul)
+    fun encenderColores(num: Int) {
         when (num) {
             0 -> btnRojo.backgroundTintList = getColorStateList(R.color.rojoClaro)
             1 -> btnVerde.backgroundTintList = getColorStateList(R.color.verdeClaro)
@@ -229,7 +187,7 @@ class MainActivity : AppCompatActivity(), IComunicador {
     /**
      * Cambia el color del botón actual por otro de tono más oscuro
      */
-    fun apagarColores (num: Int){
+    fun apagarColores(num: Int) {
         val btnAmarillo = findViewById<View>(R.id.btnAmarillo)
         val btnRojo = findViewById<View>(R.id.btnRojo)
         val btnVerde = findViewById<View>(R.id.btnVerde)
@@ -242,4 +200,56 @@ class MainActivity : AppCompatActivity(), IComunicador {
         }
     }
 
+    /**
+     * Reproduce los sonidos al pulsar los botones
+     */
+    fun reproducirSonidos(num: Int) {
+        //Comprobamos si están sonando
+        if (media1.isPlaying) {
+            media1.pause()
+            media1.seekTo(0)
+        }
+
+        if (media2.isPlaying) {
+            media2.pause()
+            media2.seekTo(0)
+        }
+
+        if (media3.isPlaying) {
+            media3.pause()
+            media3.seekTo(0)
+        }
+
+        if (media4.isPlaying) {
+            media4.pause()
+            media4.seekTo(0)
+        }
+
+        when (num) {
+            0 -> {
+                media1.setVolume(100F, 100F)
+                if (!media1.isPlaying) {
+                    media1.start()
+                }
+            }
+            1 -> {
+                media2.setVolume(100F, 100F)
+                if (!media2.isPlaying) {
+                    media2.start()
+                }
+            }
+            2 -> {
+                media3.setVolume(100F, 100F)
+                if (!media3.isPlaying) {
+                    media3.start()
+                }
+            }
+            3 -> {
+                media4.setVolume(100F, 100F)
+                if (!media4.isPlaying) {
+                    media4.start()
+                }
+            }
+        }
+    }
 }
